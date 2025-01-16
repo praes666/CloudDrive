598456958bedb072c56839886ad79a8985a9a8e0
praes666 <praes13@mail.ru>
2025-01-16 15:07:32 +0300

    /

---
+++ front/src/components/fileContainer.jsx
@@ -0,0 +1,9 @@
+export default function fileContainer(){
+    return(
+        <div className="fileContainer">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF4sQx8sfVM24rzmOEXIAVhy0BBeEbEXCl-w&s" alt=""/>
+            <p>nazvanie</p>
+            <p>size</p>
+        </div>
+    )
+}
--- front/src/components/mainpage.jsx
+++ front/src/components/mainpage.jsx
@@ -1,5 +1,28 @@
-export default function Mainpage(){
-    return(
-        <p>papapa</p>
-    )
-}
+import { useState } from 'react';
+import '../styles/styles.scss'
+import FileContainer from './fileContainer.jsx';
+
+export default function App() {
+    const [fileList, setFileList] = useState([])
+    return (
+      <div>
+        <div className="header">
+          <img className="logo" src="https://www.freeiconspng.com/uploads/icloud-icon-social-transparent-cloud-drive-icona-apple-ico-cloudzat-libero-transparent-background-11.png" alt=""/>
+          {localStorage.getItem("token") != null ? (
+            <img className="avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1OekKC4YW_f_YZUuz4TIuyMWF_homG7XZCA&s" alt=""/>
+          ) : (
+            <div className="regbtn">
+              <div>
+                <img src="https://eduprosvet.ru/image/avatar.png" alt="" />
+                <p>Профиль</p>
+              </div>
+            </div>
+          )}
+        </div>
+  
+        <div className="mainpage">
+          <FileContainer/>
+        </div>
+      </div>
+    );
+  }
