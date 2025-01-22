ea2b8cb271f7e2a6a841897f01436ffe7c83e5c6
praes666 <112814446+praes666@users.noreply.github.com>
2025-01-22 11:09:01 +0300

    Update fileContainer.jsx

--- front/src/components/fileContainer.jsx
+++ front/src/components/fileContainer.jsx
@@ -1,9 +1,8 @@
-export default function fileContainer(){
+export default function fileContainer({ file }){
     return(
         <div className="fileContainer">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF4sQx8sfVM24rzmOEXIAVhy0BBeEbEXCl-w&s" alt=""/>
-            <p>nazvanie</p>
-            <p>size</p>
+            <p>{file.name}</p>
         </div>
     )
 }
